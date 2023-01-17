bff50f66eee349d35b0d5cd5d4705e03160d6992
kdang0 <73298064+kdang0@users.noreply.github.com>
2023-01-16 23:00:28 -0500

    algos

--- 67.add-binary.js
+++ 67.add-binary.js
@@ -16,25 +16,29 @@ var addBinary = function(a, b) {
     const iterations = a.length < b.length ? a.length : b.length;
     let carryOver = 0;
     for(let i=0; i<iterations; i++){
+        console.log(sumString);
         if(a[largerLength-i] == 1 && b[iterations-1] == 1 && carryOver == 1){
-            carryOver = 0;
-            sumString += "1"
+            carryOver = 1;
+            sumString = "1" + sumString;
         }
         else if(a[largerLength-i] == 1 && b[iterations-1] == 1){
             carryOver = 1;
-            sumString += "0"
+            sumString = "0" + sumString; 
         } else if((a[largerLength-i] == 0 || b[iterations-1] ==0) && carryOver == 1){
             carryOver = 0;
-            sumString +="1";
+            sumString ="1" + sumString;
         } else{
             let sum = parseInt(a[largerLength-i]) + parseInt(b[iterations-1])
-            sumString += sum;
+            sumString = sum + sumString;
+            // console.log(sum);
         }
     }
     if(carryOver === 1){
-        sumString += "10";
+        sumString = "1" +  sumString;
     }
     return sumString;
 };
+
+console.log(addBinary("1010", "1011"));
 // @lc code=end
 
