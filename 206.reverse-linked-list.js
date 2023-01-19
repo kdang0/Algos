7d4820d4d5b4151831c6a400c74c4de208653174
kdang0 <73298064+kdang0@users.noreply.github.com>
2023-01-18 20:12:05 -0500

    algos

---
+++ 206.reverse-linked-list.js
@@ -0,0 +1,31 @@
+/*
+ * @lc app=leetcode id=206 lang=javascript
+ *
+ * [206] Reverse Linked List
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function(head) {
+    let current = head;
+    let prev = null; 
+    while(current) {
+        const temp = current.next;
+        current.next = prev; 
+        prev = current; 
+        current = temp;
+    }
+    return prev;
+};
+// @lc code=end
+
